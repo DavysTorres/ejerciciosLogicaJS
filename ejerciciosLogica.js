cfa1c20e1734dbb1c22ba0eb30b1d272e56f2fde
DavisTorres <deivis.940@gmail.com>
2024-05-06 20:20:38 -0500

    los 6 ejercicios de la guia terminados

--- ejerciciosLogica.js
+++ ejerciciosLogica.js
@@ -1,12 +1,12 @@
 //problema 1
 
 let form = document.querySelector("#problema1");
-form.addEventListener("submit", function(evento){
+form.addEventListener("submit", function (evento) {
     evento.preventDefault();
     let numero = evento.target.inputNumero.value;
     let resultado = 0;
 
-    for(let x=1; x <= numero; x++){
+    for (let x = 1; x <= numero; x++) {
         resultado += x
     }
     document.querySelector("#resultado1").innerHTML = resultado
@@ -15,14 +15,13 @@ form.addEventListener("submit", function(evento){
 //problema 2
 
 let form1 = document.querySelector("#problema2");
-form1.addEventListener("submit", function(evento) {
+form1.addEventListener("submit", function (evento) {
     evento.preventDefault();
     let numero = evento.target.inputnumP.value;
-    
-    if(numero%2===0)
-    {
+
+    if (numero % 2 === 0) {
         document.querySelector("#resultado2").innerHTML = "El numero es par"
-    }else{
+    } else {
         document.querySelector("#resultado2").innerHTML = "El numero es impar"
     }
 })
@@ -44,27 +43,54 @@ form2.addEventListener("submit", function (evento) {
         } else if (edad >= 18 && genero == 'Mujer') {
             mensaje += "Has ganado: **cerveza** y **Porción de pizza Hawaiana** "
         } else if (edad >= 18 && genero == 'Hombre') {
-                mensaje += "Has ganado: **cerveza** y **Porción de pizza de tres carnes** "
+            mensaje += "Has ganado: **cerveza** y **Porción de pizza de tres carnes** "
         }
         if (mensaje == "") {
-        mensaje = "Desafortunadamente, **no recibes ningún premio** "
+            mensaje = "Desafortunadamente, **no recibes ningún premio** "
         }
     }
     document.querySelector("#resultado3").innerText = mensaje
 });
 
 //problema 4
+let menu = [
+    { item: 'pizza', precio: 15 },
+    { item: 'hamburguesa', precio: 10 },
+    { item: 'refresco', precio: 5 }
+];
 
+let divsito = document.querySelector("#menuPlatos")
+menu.forEach(plato => {
+    divsito.innerHTML += `<div> ${plato.item} - $${plato.precio} </div>`
+});
 
+let total = 0;
+document.querySelector("#problema4").addEventListener("submit", function (evento) {
+    console.log("pepe");
+    evento.preventDefault();
+    let item = evento.target.inputMenu.value;
+
+    if (item == "") {
+        document.querySelector("#resultado4").innerHTML = "Por favor, escoge un producto del menu.";
+    } else {
+        if (item != "pagar") {
+            let producto = menu.find(objItem => objItem.item === item);
+            total += producto.precio
+            evento.target.reset()
+        } else {
+                document.querySelector("#resultado4").innerHTML = `El total a pagar es: $${total}`;
+            }
+    }
+});
 
 //problema 5
 let form4 = document.querySelector("#problema5");
 form4.addEventListener("submit", function (evento) {
     evento.preventDefault();
     let multiplicador = evento.target.inputMultiplicador.value
-    let  total = ''
+    let total = ''
 
-    for(let x=1; x <= 10; x++){
+    for (let x = 1; x <= 10; x++) {
         tblaMultiplicar = multiplicador * x
         total += x + " x " + multiplicador + " = " + tblaMultiplicar + "<br>";
 
@@ -77,15 +103,40 @@ let form5 = document.querySelector("#problema6");
 form5.addEventListener("submit", function (evento) {
     evento.preventDefault();
     let promedio = evento.target.inputPromedio.value;
-    let matricula ;
+    let matricula;
 
     if (promedio < 3) {
         matricula = 1000000; // No se aplica descuento
-      } else if (promedio >= 3 && promedio <= 4) {
+    } else if (promedio >= 3 && promedio <= 4) {
         matricula = 1000000 - (1000000 * 0.05); // Se descuenta el 5%
-      } else {
+    } else {
         matricula = 1000000 - (1000000 * 0.5); // Se descuenta el 50%
-      }
+    }
     document.querySelector("#resultado6").innerHTML = `Valor de matrícula a pagar: $${matricula}`;
 
 })
+
+
+
+
+
+/* function agregarAlMenu(item, precio) {
+    menu.push({item: item, precio: precio});
+}
+
+function agregarAlCarrito(item) {
+    let producto = menu.find(producto => producto.item === item);
+    if (producto) {
+        carrito.push(producto);
+        total += producto.precio;
+    } else {
+        console.error(`Lo siento, ${item} no está en el menú.`);
+    }
+}
+
+function pagar() {
+    console.log(`El total a pagar es: $${total}`);
+    carrito = [];
+    total = 0; */
+
+
